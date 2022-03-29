bf9342441d045ef327939e97403dce82de72738e
Jun-Hee Lee <flyingmt@gmail.com>
2022-03-29 14:14:42 +0900

    Improving our WS endpoint, and setting up go routines

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/flyingmt/golang_websockets/internal/handlers"
 )
 
 func main() {
    mux := routes()
+
+   log.Println("Starting channel listener")
+   go handlers.ListenToWsChannel()
 
    log.Println("Staring web server on port 8080")
 
